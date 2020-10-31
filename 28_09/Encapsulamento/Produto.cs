a0dc31b7d6469731971457d357c57fb3228b96ee
melinavilassa123 <65971962+melinavilassa123@users.noreply.github.com>
2020-10-30 21:37:55 -0300

    Add files via upload

---
+++ 28_09/Encapsulamento/Produto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1_Produto__Encapsulamento_
+{
+	class Produto
+	{
+		//declaração de variáveis, duas com propriedades autoimplementadas
+		private string _nome;
+		public double Preco { get; private set; }
+		public int Quantidade { get; private set; }
+
+		//construtor padrão
+		public Produto() 
+		{
+		}
+
+		//construtor personalizado
+		public Produto(string nome, double preco, int quantidade)
+		{
+			_nome = nome;
+			Preco = preco;
+			Quantidade = quantidade;
+		}
+
+		//propriedade Nome customizada com condição
+		public string Nome
+		{
+			get { return _nome; }
+
+			set { if (value != null && value.Length > 1)
+				{
+					_nome = value;
+				}
+			}
+		}
+
+		//métodos de classe
+		public double ValorTotalEstoque()
+		{
+			double total = Quantidade * Preco;
+			return total;
+		}
+
+		public void AdicionarProdutos(int qnt)
+		{
+			Quantidade += qnt;
+		}
+
+		public void RemoverProdutos(int qnt)
+		{
+			Quantidade -= qnt;
+		}
+
+		//tostring para definir a formatação
+		public override string ToString()
+		{
+			return "Nome: " + _nome + "\n"
+				+ "Preço: R$ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + "\n"
+				+ Quantidade + " unidades em estoque\n"
+				+ "Valor total: R$ " + ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
+		}
+	}
+}
---
+++ 28_09/Encapsulamento/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            Console.Write("Digite o nome do produto: ");
+            string nome = Console.ReadLine();
+            Console.Write("Digite o preço do produto em reais: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite a quantidade em estoque do produto: ");
+            int quantidade = int.Parse(Console.ReadLine());   
+            Produto p = new Produto(nome, preco, quantidade);          
+            Console.WriteLine("\nDados do produto \n" + p + "\n\n");          
+            Console.Write("Digite o quantidade a ser adicionada ao estoque: ");
+            p.AdicionarProdutos(int.Parse(Console.ReadLine()));          
+            Console.WriteLine($"\nDados atualizados do produto \n" + p + "\n\n");
+            Console.Write("Digite o quantidade a ser removida ao estoque: ");
+            p.RemoverProdutos(int.Parse(Console.ReadLine()));         
+            Console.WriteLine($"\nDados atualizados do produto \n" + p + "\n\n");
+            Console.ReadKey();
+        }
+    }
+}
