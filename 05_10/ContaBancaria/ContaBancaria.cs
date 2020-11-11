499e2aafca49fbd3e682c093bfed5c432d301f37
melinavilassa123 <65971962+melinavilassa123@users.noreply.github.com>
2020-11-11 10:40:14 -0300

    Add files via upload

---
+++ 05_10/ContaBancaria/ContaBancaria.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    class ContaBancaria
+    {
+        
+        public int Numero { get; set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+       
+        public ContaBancaria()
+        {
+        }       
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }       
+        public void RealizarDeposito(double valor)
+        {
+            Saldo += valor;
+        }
+        public void RealizarSaque(double valor)
+        {
+            Saldo -= valor + 5;
+        }
+        public override string ToString()
+        {
+            return  "\nConta   - " + Numero.ToString(CultureInfo.InvariantCulture)
+                    + "\nTitular - " + Titular
+                    + "\nSaldo   - R$ " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ 05_10/ExemploStruct/Struct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExemploStruct
+{
+    struct Struct
+    {
+        public double X, Y;
+
+        public override string ToString()
+        {
+            return "(" + X + "," + Y + ")";
+        }
+    }
+}
