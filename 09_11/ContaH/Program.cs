053cf62ea21b055b6e6fb687494541c97f73b6e7
melinavilassa123 <65971962+melinavilassa123@users.noreply.github.com>
2020-11-23 17:24:24 -0300

    Add files via upload

---
+++ 09_11/ContaH/Program.cs
@@ -0,0 +1,15 @@
+﻿using _3_Conta.Entities;
+using System;
+
+namespace ContaH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaJuridica conta = new ContaJuridica(8010, "Ana", 100, 500);
+
+            Console.WriteLine(conta.Saldo);
+        }
+    }
+}
